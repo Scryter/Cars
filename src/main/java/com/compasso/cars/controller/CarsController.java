2f6604748b6d4c7d6c188c229628edf76aa97dba
Josiel <josielcardosomercio@gmail.com>
2021-09-05 00:17:52 -0300

    Update CarsControll
    O conceito de um código de filtro multi-parâmetros para o método Get

--- src/main/java/com/compasso/cars/controller/CarsController.java
+++ src/main/java/com/compasso/cars/controller/CarsController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -40,6 +41,16 @@ public class CarsController
 		return CarDto.converter(cars);
 	}
 	
+	//=====================================
+	/*@GetMapping
+	public ResponseEntity getThings(@RequestParam("filter") String[] filters) 
+	{
+		
+	    return ResponseEntity.ok(filters);
+	}*/
+	
+	
+	//======================================
 	@PostMapping
 	public ResponseEntity <CarDto> cadastrar(@RequestBody @Valid CarForm form, UriComponentsBuilder uriBuilder)
 	{
